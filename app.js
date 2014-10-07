504045b53e0b2bcb2c90e2fb0873dd67293aa47d
LandonWest <landonwest5@gmail.com>
2014-10-06 21:46:27 -0700

    add images array with full and thumb images

--- app.js
+++ app.js
@@ -7,20 +7,33 @@
   });
 
   var gems = [
-  {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: 'A very special gem for your viewing pleasure!',
-    canPurchase: true,
-    soldOut: false
-  },
-  {
-    name: 'Pentagonal Gem',
-    price: 5.95,
-    description: 'So very nice, you\'ll stare at it all day!',
-    canPurchase: false,
-    soldOut: false
-  }
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: 'A very special gem for your viewing pleasure!',
+      canPurchase: true,
+      soldOut: false,
+      images: [
+        {
+          full: "img/dodecahedron-01-full.jpg",
+          thumb: "img/dodecahedron-01-thumb.jpg"
+        }
+      ]
+    },
+
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: 'So very nice, you\'ll stare at it all day!',
+      canPurchase: true,
+      soldOut: false,
+      images: [
+        {
+          full: "img/dodecahedron-02-full.jpg",
+          thumb: "img/dodecahedron-02-thumb.jpg"
+        }
+      ]
+    }
   ];
 
 })();
